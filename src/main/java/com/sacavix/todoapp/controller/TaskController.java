208698ac59a5764bed67832f4a0f3fa0b2e3f02a
Magdiel Vega <mvg140386@gmail.com>
2023-12-30 00:35:51 +0100

    Inicializacion del proyecto con configuraciones listas.

---
+++ src/main/java/com/sacavix/todoapp/controller/TaskController.java
@@ -0,0 +1,8 @@
+package com.sacavix.todoapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+}
