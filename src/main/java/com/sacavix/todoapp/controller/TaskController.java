262ede9e186b2c1ce6db07505dc88e3a2a4181e8
Magdiel Vega <mvg140386@gmail.com>
2023-12-30 16:49:37 +0100

    Función crear tarea terminada.

--- src/main/java/com/sacavix/todoapp/controller/TaskController.java
+++ src/main/java/com/sacavix/todoapp/controller/TaskController.java
@@ -1,8 +1,23 @@
 package com.sacavix.todoapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sacavix.todoapp.persistence.entity.Task;
+import com.sacavix.todoapp.service.TaskService;
+import com.sacavix.todoapp.service.dto.TaskInDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskInDTO){
+        return this.taskService.createTask(taskInDTO);
+
+    }
 }
